66e4b0ca46601fc3ad1303251b752e102bcfe85b
PullaTharunkumar <tharunkumar20004@gmail.com>
2023-11-16 20:17:07 +0530

    reset and othe changes made

--- src/controllers/fileUpload.controller.js
+++ src/controllers/fileUpload.controller.js
@@ -1,15 +1,13 @@
 import { csvFileUpload, getFileNames, getFileDetails } from "../models/csvUpload.model.js";
-import fs from 'fs'
-import csvParser from 'csv-parser';
 
 export default class FileUploadController {
     // Home Page
     async homePage(req, res) {
-        try{
+        try {
             const files = await getFileNames()
-            res.render('home', { title: 'CSV File Upload', files })
-        }catch(err){
-            console.log('Error in csvFileUploadPage function',err);
+            return res.render('home', { title: 'CSV File Upload', files })
+        } catch (err) {
+            console.log('Error in csvFileUploadPage function', err);
         }
     }
     // File upload Function
@@ -29,23 +27,7 @@ export default class FileUploadController {
     async fileOpen(req, res) {
         try {
             const fileInfo = await getFileDetails(req.params.id)
-            const headers = []
-            const fileData = []
-            // stream data
-            const csvStream = fs.createReadStream(fileInfo.filePath).pipe(csvParser())
-            csvStream.on('headers', (data) => {
-                    data.map((header) => {
-                    headers.push(header);
-                })
-            })
-                .on('data', (data) => fileData.push(data))
-                .on('end', () => {
-                    console.log('CSV parsing complete');
-                    console.log('Headers:', headers);
-                    console.log('Data:', fileData)
-                    return res.render('fileDetails', { title: 'File Details', fileData, headers,fileName : fileInfo.fileName })
-                })
-            console.log(headers, fileData);
+            return res.render('fileDetails', { title: 'File Details', fileData: fileInfo.fileData, headers: fileInfo.headers, fileName: fileInfo.fileName, id: req.params.id })
         } catch (err) {
             console.log('Error getting in fileOpen function', err);
         }
--- src/models/csvUpload.model.js
+++ src/models/csvUpload.model.js
@@ -1,39 +1,56 @@
-import { ObjectId } from 'mongodb';
 import { Collection } from '../../config/mongodb.js';
+import fs from 'fs'
+import csvParser from 'csv-parser';
+import { ObjectId } from 'mongodb';
 
 //Fetching files information from MongoDB
 export const getFileNames = async () => {
-    try{
+    try {
         const collection = Collection();
         const result = await collection.find().toArray()
         return result
-    }catch(err){
-        console.log('error while fetching files information from MongoDB',err);
+    } catch (err) {
+        console.log('error while fetching files information from MongoDB', err);
     }
 }
 // uploading file Information in MongoDB
 export const csvFileUpload = async (fileInfo) => {
-    try{
+    try {
         const collection = Collection();
+        // stream data
+        const headers = []
+        const fileData = []
+        const csvStream = fs.createReadStream(fileInfo.path).pipe(csvParser())
+        csvStream.on('headers', (data) => {
+            data.map((header) => {
+                headers.push(header);
+            })
+        })
+            .on('data', (data) => fileData.push(data))
+            .on('end', async () => resolve())
+        await new Promise((resolve) => csvStream.on('end', resolve));
         const data = {
             fileName: fileInfo.originalname,
             filePath: fileInfo.path,
             file: fileInfo.filename,
-            timeStamp : Date.now()
+            headers,
+            fileData,
+            timeStamp: Date.now()
         }
         await collection.insertOne(data)
         console.log('File Uploaded');
-    }catch(err){
-        console.log('error whlie uploading file Information in MongoDB',err);
+
+    } catch (err) {
+        console.log('error whlie uploading file Information in MongoDB', err);
     }
 }
 // getting file information from MongoDB
 export const getFileDetails = async (id) => {
-    try{
+    try {
         const collection = Collection();
-        const result = await collection.findOne({ _id : new ObjectId(id) })
+        const result = await collection.findOne({ _id: new ObjectId(id) })
         return result
-    }catch(err){
-        console.log('Error while getting file information from MongoDB',err);
+    } catch (err) {
+        console.log('Error while getting file information from MongoDB', err);
     }
 }
